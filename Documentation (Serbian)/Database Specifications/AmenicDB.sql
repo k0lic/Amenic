dd9c086d19f7e3222c59eece42d5887653b9fd84
Rpsaman13000 <mitrovic.martin@gmail.com>
2020-05-04 02:29:23 +0200

    Amenic.sql script ready to be imported to phpmyadmin. Start data also added.

--- Documentation (Serbian)/Database Specifications/AmenicDB.sql
+++ Documentation (Serbian)/Database Specifications/AmenicDB.sql
@@ -1,9 +1,12 @@
 
 CREATE TABLE Admins
 (
-	email                VARCHAR(255) NOT NULL,
-	firstName            VARCHAR(50) NOT NULL,
-	lastName             VARCHAR(50) NOT NULL
+	email VARCHAR
+	(255) NOT NULL,
+	firstName VARCHAR
+	(64) NOT NULL,
+	lastName VARCHAR
+	(64) NOT NULL
 );
 
 ALTER TABLE Admins
@@ -11,20 +14,20 @@ ADD CONSTRAINT XPKAdmins PRIMARY KEY (email);
 
 CREATE TABLE Cinemas
 (
-	email                VARCHAR(255) NOT NULL,
-	name                 VARCHAR(64) NOT NULL,
-	address              VARCHAR(64) NOT NULL,
-	phoneNumber          VARCHAR(64) NOT NULL,
-	description          TEXT NULL,
-	mngFirstName         VARCHAR(64) NOT NULL,
-	mngLastName          VARCHAR(64) NOT NULL,
-	mngPhoneNumber       VARCHAR(64) NOT NULL,
-	mngEmail             VARCHAR(255) NOT NULL,
-	banner               VARBINARY NULL,
-	approved             boolean NOT NULL,
-	closed               boolean NOT NULL,
-	idCountry            BIGINT NULL,
-	idCity               BIGINT NULL
+	email VARCHAR(255) NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	address VARCHAR(64) NOT NULL,
+	phoneNumber VARCHAR(64) NOT NULL,
+	description TEXT,
+	mngFirstName VARCHAR(64) NOT NULL,
+	mngLastName VARCHAR(64) NOT NULL,
+	mngPhoneNumber VARCHAR(64) NOT NULL,
+	mngEmail VARCHAR(255) NOT NULL,
+	banner VARBINARY(60000),
+	approved boolean NOT NULL,
+	closed boolean NOT NULL,
+	idCountry BIGINT,
+	idCity BIGINT
 );
 
 ALTER TABLE Cinemas
@@ -32,18 +35,17 @@ ADD CONSTRAINT XPKCinemas PRIMARY KEY (email);
 
 CREATE TABLE Cities
 (
-	idCity               BIGINT NOT NULL,
-	name                 VARCHAR(50) NOT NULL,
-	idCountry            BIGINT NOT NULL
+	idCity BIGINT NOT NULL
+	AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR
+	(64) NOT NULL,
+	idCountry BIGINT NOT NULL
 );
-
-ALTER TABLE Cities
-ADD CONSTRAINT XPKCities PRIMARY KEY (idCity);
 
 CREATE TABLE ComingSoon
 (
-	tmdbID               VARCHAR(64) NOT NULL,
-	email                VARCHAR(255) NOT NULL
+	tmdbID VARCHAR(64) NOT NULL,
+	email VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE ComingSoon
@@ -51,18 +53,17 @@ ADD CONSTRAINT XPKComingSoon PRIMARY KEY (tmdbID,email);
 
 CREATE TABLE Countries
 (
-	idCountry            BIGINT NOT NULL,
-	name                 VARCHAR(50) NOT NULL
+	idCountry BIGINT NOT NULL
+	AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR
+	(64) NOT NULL
 );
-
-ALTER TABLE Countries
-ADD CONSTRAINT XPKCountries PRIMARY KEY (idCountry);
 
 CREATE TABLE Galleries
 (
-	email                VARCHAR(255) NOT NULL,
-	name                 VARCHAR(64) NOT NULL,
-	image                VARBINARY NOT NULL
+	email VARCHAR(255) NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	image VARBINARY(60000) NOT NULL
 );
 
 ALTER TABLE Galleries
@@ -70,22 +71,21 @@ ADD CONSTRAINT XPKGalleries PRIMARY KEY (email,name);
 
 CREATE TABLE Movies
 (
-	tmdbID               VARCHAR(64) NOT NULL,
-	title                VARCHAR(64) NOT NULL,
-	released             DATE NOT NULL,
-	runtime              INTEGER NOT NULL,
-	genre                TEXT NOT NULL,
-	director             TEXT NOT NULL,
-	writer               TEXT NOT NULL,
-	actors               TEXT NOT NULL,
-	plot                 TEXT NULL,
-	poster               VARCHAR(255) NULL,
-	backgroundImg        VARCHAR(255) NULL,
-	imdbRating           DECIMAL(2) NULL CHECK ( <=10
- ),
-	imdbID               VARCHAR(64) NOT NULL,
-	reviews              TEXT NULL,
-	trailer              VARCHAR(255) NULL
+	tmdbID VARCHAR(64) NOT NULL,
+	title VARCHAR(64) NOT NULL,
+	released DATE NOT NULL,
+	runtime INTEGER NOT NULL,
+	genre TEXT NOT NULL,
+	director TEXT NOT NULL,
+	writer TEXT NOT NULL,
+	actors TEXT NOT NULL,
+	plot TEXT,
+	poster VARCHAR(255) NULL,
+	backgroundImg VARCHAR(255) NULL,
+	imdbRating DECIMAL(2) CHECK ( imdbRating <= 10 ),
+	imdbID VARCHAR(64) NOT NULL,
+	reviews TEXT,
+	trailer VARCHAR(255)
 );
 
 ALTER TABLE Movies
@@ -93,36 +93,37 @@ ADD CONSTRAINT XPKMovies PRIMARY KEY (tmdbID);
 
 CREATE TABLE Projections
 (
-	idPro                BIGINT NOT NULL,
-	roomName             VARCHAR(64) NOT NULL,
-	email                VARCHAR(255) NOT NULL,
-	dateTime             DATE NOT NULL,
-	price                DECIMAL(19,4) NOT NULL,
-	canceled             boolean NOT NULL,
-	tmdbID               VARCHAR(64) NOT NULL,
-	idTech               INTEGER NOT NULL
+	idPro BIGINT NOT NULL
+	AUTO_INCREMENT PRIMARY KEY,
+	roomName VARCHAR
+	(64) NOT NULL,
+	email VARCHAR
+	(255) NOT NULL,
+	dateTime DATE NOT NULL,
+	price DECIMAL
+	(19,4) NOT NULL,
+	canceled boolean NOT NULL,
+	tmdbID VARCHAR
+	(64) NOT NULL,
+	idTech INTEGER NOT NULL
 );
-
-ALTER TABLE Projections
-ADD CONSTRAINT XPKProjections PRIMARY KEY (idPro);
 
 CREATE TABLE Reservations
 (
-	idRes                BIGINT NOT NULL,
-	confirmed            boolean NOT NULL,
-	idPro                BIGINT NOT NULL,
-	email                VARCHAR(255) NOT NULL
+	idRes BIGINT NOT NULL
+	AUTO_INCREMENT PRIMARY KEY,
+	confirmed boolean NOT NULL,
+	idPro BIGINT NOT NULL,
+	email VARCHAR
+	(255) NOT NULL
 );
-
-ALTER TABLE Reservations
-ADD CONSTRAINT XPKReservations PRIMARY KEY (idRes);
 
 CREATE TABLE Rooms
 (
-	name                 VARCHAR(64) NOT NULL,
-	email                VARCHAR(255) NOT NULL,
-	numberOfRows         INTEGER NOT NULL,
-	seatsInRow           INTEGER NOT NULL
+	name VARCHAR(64) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	numberOfRows INTEGER NOT NULL,
+	seatsInRow INTEGER NOT NULL
 );
 
 ALTER TABLE Rooms
@@ -130,9 +131,9 @@ ADD CONSTRAINT XPKRooms PRIMARY KEY (name,email);
 
 CREATE TABLE RoomTechnologies
 (
-	name                 VARCHAR(64) NOT NULL,
-	email                VARCHAR(255) NOT NULL,
-	idTech               INTEGER NOT NULL
+	name VARCHAR(64) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	idTech INTEGER NOT NULL
 );
 
 ALTER TABLE RoomTechnologies
@@ -140,12 +141,12 @@ ADD CONSTRAINT XPKRoomTechnologies PRIMARY KEY (name,email,idTech);
 
 CREATE TABLE RUsers
 (
-	email                VARCHAR(255) NOT NULL,
-	firstName            VARCHAR(64) NOT NULL,
-	lastName             VARCHAR(64) NOT NULL,
-	phoneNumber          VARCHAR(64) NULL,
-	idCountry            BIGINT NULL,
-	idCity               BIGINT NULL
+	email VARCHAR(255) NOT NULL,
+	firstName VARCHAR(64) NOT NULL,
+	lastName VARCHAR(64) NOT NULL,
+	phoneNumber VARCHAR(64),
+	idCountry BIGINT,
+	idCity BIGINT
 );
 
 ALTER TABLE RUsers
@@ -153,11 +154,11 @@ ADD CONSTRAINT XPKRUsers PRIMARY KEY (email);
 
 CREATE TABLE Seats
 (
-	idPro                BIGINT NOT NULL,
-	rowNumber            INTEGER NOT NULL,
-	seatNumber           INTEGER NOT NULL,
-	status               VARCHAR(64) NOT NULL,
-	idRes                BIGINT NULL
+	idPro BIGINT NOT NULL,
+	rowNumber INTEGER NOT NULL,
+	seatNumber INTEGER NOT NULL,
+	status VARCHAR(64) NOT NULL,
+	idRes BIGINT
 );
 
 ALTER TABLE Seats
@@ -165,18 +166,17 @@ ADD CONSTRAINT XPKSeats PRIMARY KEY (idPro,rowNumber,seatNumber);
 
 CREATE TABLE Technologies
 (
-	idTech               INTEGER NOT NULL,
-	name                 VARCHAR(64) NOT NULL
+	idTech INTEGER NOT NULL
+	AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR
+	(64) NOT NULL
 );
-
-ALTER TABLE Technologies
-ADD CONSTRAINT XPKTechnologies PRIMARY KEY (idTech);
 
 CREATE TABLE Users
 (
-	email                VARCHAR(255) NOT NULL,
-	password             VARCHAR(255) NOT NULL,
-	image                VARBINARY NULL
+	email VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	image VARBINARY(60000)
 );
 
 ALTER TABLE Users
@@ -184,9 +184,9 @@ ADD CONSTRAINT XPKUsers PRIMARY KEY (email);
 
 CREATE TABLE Verifications
 (
-	email                VARCHAR(255) NOT NULL,
-	code                 VARCHAR(64) NOT NULL,
-	created              TIMESTAMP NOT NULL
+	email VARCHAR(255) NOT NULL,
+	code VARCHAR(64) NOT NULL,
+	created TIMESTAMP NOT NULL
 );
 
 ALTER TABLE Verifications
@@ -194,10 +194,10 @@ ADD CONSTRAINT XPKVerifications PRIMARY KEY (email);
 
 CREATE TABLE Workers
 (
-	email                VARCHAR(255) NOT NULL,
-	idCinema             VARCHAR(255) NOT NULL,
-	firstName            VARCHAR(50) NOT NULL,
-	lastName             VARCHAR(50) NOT NULL
+	email VARCHAR(255) NOT NULL,
+	idCinema VARCHAR(255) NOT NULL,
+	firstName VARCHAR(50) NOT NULL,
+	lastName VARCHAR(50) NOT NULL
 );
 
 ALTER TABLE Workers
@@ -205,11 +205,11 @@ ADD CONSTRAINT XPKWorkers PRIMARY KEY (email);
 
 ALTER TABLE Admins
 ADD CONSTRAINT R_9 FOREIGN KEY (email) REFERENCES Users (email)
-		ON DELETE CASCADE;
+ON DELETE CASCADE;
 
 ALTER TABLE Cinemas
 ADD CONSTRAINT R_10 FOREIGN KEY (email) REFERENCES Users (email)
-		ON DELETE CASCADE;
+ON DELETE CASCADE;
 
 ALTER TABLE Cinemas
 ADD CONSTRAINT R_21 FOREIGN KEY (idCountry) REFERENCES Countries (idCountry);
@@ -255,7 +255,7 @@ ADD CONSTRAINT R_29 FOREIGN KEY (idTech) REFERENCES Technologies (idTech);
 
 ALTER TABLE RUsers
 ADD CONSTRAINT R_11 FOREIGN KEY (email) REFERENCES Users (email)
-		ON DELETE CASCADE;
+ON DELETE CASCADE;
 
 ALTER TABLE RUsers
 ADD CONSTRAINT R_19 FOREIGN KEY (idCountry) REFERENCES Countries (idCountry);
@@ -274,7 +274,37 @@ ADD CONSTRAINT R_8 FOREIGN KEY (email) REFERENCES Users (email);
 
 ALTER TABLE Workers
 ADD CONSTRAINT R_12 FOREIGN KEY (email) REFERENCES Users (email)
-		ON DELETE CASCADE;
+ON DELETE CASCADE;
 
 ALTER TABLE Workers
 ADD CONSTRAINT R_18 FOREIGN KEY (idCinema) REFERENCES Cinemas (email);
+
+INSERT INTO Countries(name) 
+VALUES("Srbija");
+
+INSERT INTO Cities(name,idCountry) 
+VALUES("Beograd",1);
+
+INSERT INTO Cities(name,idCountry) 
+VALUES("Novi Sad",1);
+
+INSERT INTO Cities(name, idCountry) 
+VALUES("Niš",1);
+
+INSERT INTO Users(email, password)
+VALUES("andrija@gmail.com", "root");
+
+INSERT INTO Users(email, password)
+VALUES("milos@gmail.com", "root");
+
+INSERT INTO Users(email, password)
+VALUES("martin@gmail.com", "root");
+
+INSERT INTO Admins(email, firstName, lastName)
+VALUES("andrija@gmail.com", "Andrija", "Kolić");
+
+INSERT INTO Admins(email, firstName, lastName)
+VALUES("milos@gmail.com", "Miloš", "Živković");
+
+INSERT INTO Admins(email, firstName, lastName)
+VALUES("martin@gmail.com", "Martin", "Mitrović");
