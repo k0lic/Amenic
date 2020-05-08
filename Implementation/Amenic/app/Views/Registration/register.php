a513ef864f4c039e1e394f9ecc0ff8bfe52f06be
Milos Zivkovic <zm170625d@student.etf.bg.ac.rs>
2020-05-08 13:58:53 +0200

    Started implementing user registration

---
+++ Implementation/Amenic/app/Views/Registration/register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+		<title>Amenic - Register</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="registerBar">
+				<img src="./imgs/logo.svg" class="logo" alt="Amenic" />
+				<div class="registerSteps">
+					<div class="registerBarTitle">Thank you for signing up.</div>
+					<div class="registerBarSubtitle mt-3">
+						Let us know what kind of account you would like to create.
+					</div>
+				</div>
+			</div>
+			<div class="registerWrapper centerRow">
+				<div class="registerRow centerRow">
+					<img src="./assets/user.svg" class="icon" />
+
+					<div class="registerType">
+						<span class="registerTypeTitle">Individual</span>
+						<span class="registerTypeDesc"
+							>I want to create an account to reserve tickets.</span
+						>
+					</div>
+
+					<img src="./assets/next.svg" class="icon" />
+				</div>
+
+				<div class="registerRow centerRow mt-5">
+					<img src="./assets/clapper.svg" class="icon" />
+
+					<div class="registerType">
+						<span class="registerTypeTitle">Cinema</span>
+						<span class="registerTypeDesc">
+							I want to create an account for my cinema.
+						</span>
+					</div>
+
+					<img src="./assets/next.svg" class="icon" />
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
