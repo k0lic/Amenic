588a8651fe221d819ee7f88deb83edef73f08210
Rpsaman13000 <mitrovic.martin@gmail.com>
2020-05-16 13:53:21 +0200

    Admin side menu fixed

--- Implementation/Amenic/app/Views/SideMenus/AdminSideMenu.php
+++ Implementation/Amenic/app/Views/SideMenus/AdminSideMenu.php
@@ -3,12 +3,12 @@
         <a href="/AdminController/users" class="<?php if(strcmp($actMenu,"0")==0) echo "activeMenu";?>" >Users</a>
     </li>
     <li>
-        <a href="/AdminController/cinemas" class="<?php if(strcmp($actMenu,"0")==1) echo "activeMenu";?>">Cinemas</a>
+        <a href="/AdminController/cinemas" class="<?php if(strcmp($actMenu,"1")==0) echo "activeMenu";?>">Cinemas</a>
     </li>
     <li>
-        <a href="/AdminController/requests" class="<?php if(strcmp($actMenu,"0")==2) echo "activeMenu";?>">Requests</a>
+        <a href="/AdminController/requests" class="<?php if(strcmp($actMenu,"2")==0) echo "activeMenu";?>">Requests</a>
     </li>
     <li>
-        <a href="/AdminController/admins" class="<?php if(strcmp($actMenu,"0")==3) echo "activeMenu";?>">Admins</a>
+        <a href="/AdminController/admins" class="<?php if(strcmp($actMenu,"3")==0) echo "activeMenu";?>">Admins</a>
     </li>
 </ul>
